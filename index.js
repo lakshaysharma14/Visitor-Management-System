0a213e3be974677002d5e634c60f8ca812505150
Lakshay Sharma <32371154+lakshaysharma14@users.noreply.github.com>
2021-01-28 21:11:24 +0530

    Mail Options Editted

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 const config = require("./config.json");
-//use path module
 const path = require("path");
-//use express module
 const express = require("express");
-//use hbs view engine
-const hbs = require("hbs");
-//use bodyParser middleware
-const bodyParser = require("body-parser");
-//use mysql database
-const mysql = require("mysql");
-const app = express();
+const hbs = require("hbs"); //use hbs view engine
+const bodyParser = require("body-parser");//use bodyParser middleware
+const mysql = require("mysql"); //use mysql database
 
+const app = express();
 var nodemailer = require("nodemailer");
 
-//Create Connection
+//____________________________ Establishing and Setting All Connections____________________
+
+// Create Connection ( All these details are stored in a separate config.js file)
 const conn = mysql.createConnection({
   host: config.database.host_name,
   user: config.database.username,
@@ -21,10 +18,12 @@ const conn = mysql.createConnection({
   database: config.database.database_name,
 });
 
+// Setting Credentials for Twillo API 
 const accountSid = config.twilio.accountSid;
 const authToken = config.twilio.authToken;
 const client = require("twilio")(accountSid, authToken);
 
+// Setting Nodemailer Transporter
 var transporter = nodemailer.createTransport({
   host: config.email_setting.host,
   port: 587,
@@ -36,22 +35,25 @@ var transporter = nodemailer.createTransport({
   },
 });
 
-//connect to database
+//Connecting to Mysql Database
 conn.connect((err) => {
   if (err) throw err;
   console.log("Mysql Connected...");
 });
 
-//set views file
-app.set("views", path.join(__dirname, "views"));
-//set view engine
-app.set("view engine", "hbs");
+// Setting Configuration for Using Handlebars(hbs)
+app.set("views", path.join(__dirname, "views"));//set views file
+app.set("view engine", "hbs");//set view engine
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-//set folder public as static folder for static file
-app.use("/assets", express.static(__dirname + "/public"));
 
-//route for homepage
+app.use("/assets", express.static(__dirname + "/public"));//set folder public as static folder for static file
+
+//______________________________Different Routes ___________________________________________
+
+//***Homepage***
+
 app.get("/", (req, res) => {
   let sql = "SELECT * FROM visitors";
   let query = conn.query(sql, (err, results) => {
@@ -62,79 +64,67 @@ app.get("/", (req, res) => {
   });
 });
 
-//route for insert data
+
+//***Inserting User Details in Database***
+
 app.post("/save", (req, res) => {
-  let data = {
+  
+  let data =
+  {
     name: req.body.name,
     email_id: req.body.email_id,
     checkin: req.body.checkin,
     mobile_no: req.body.mobile_no,
   };
+  
   let sql = "INSERT INTO visitors SET ?";
-  let query = conn.query(sql, data, (err, results) => {
-    if (err) throw err;
-
-    let sql1 = "SELECT * FROM visitors WHERE id=" + results.insertId;
-    let query1 = conn.query(sql1, (err, result) => {
+  let query = conn.query(sql,data,(err, results) =>{
       if (err) throw err;
-      console.log(result);
-      let htmlBody = "New visitor information : \n";
-      htmlBody +=
-        "Name : " +
-        result[0].name +
-        " \n " +
-        "\n" +
-        " Email : " +
-        result[0].email_id +
-        " \n " +
-        "\n" +
-        "Mobile Number : " +
-        result[0].mobile_no +
-        " \n " +
-        "\n" +
-        " Check In Date Time :" +
-        result[0].checkin;
-      var mailOptions = {
-        from: config.host.email,
-        to: config.host.email,
-        subject: "New visitor has joined.",
-        html: htmlBody,
-      };
-      transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-          console.log(error);
-        } else {
-          let MobileBody = "New visitor information : ";
-          MobileBody +=
-            "Name : " +
-            data.name +
-            " " +
-            " Email : " +
-            data.email_id +
-            " " +
-            "Mobile Number : " +
-            data.mobile_no +
-            " " +
-            " Check In Date Time :" +
-            data.checkin;
-          client.messages.create(
-            {
-              body: MobileBody,
-              from: config.twilio.from_number,
-              to: config.twilio.to_number,
-            },
-            function (error, info) {
-              if (error) {
-                console.log(error);
-              } else {
-                console.log("Message sent: " + info);
+      
+      let sql1 = "SELECT * FROM visitors WHERE id=" + results.insertId;
+      let query1 = conn.query(sql1, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        let htmlBody = "New visitor information : \n";
+        htmlBody += "Name : " + result[0].name + " \n " + "\n" + 
+        " Email : " +  result[0].email_id + " \n " + "\n" +
+        "Mobile Number : " + result[0].mobile_no + " \n " + "\n" +
+        " Check In Date Time :" + result[0].checkin;
+      
+        var mailOptions = 
+        {
+          from: config.host.email,
+          to: config.host.email,
+          subject: "New visitor has joined.",
+          html: htmlBody,
+        };
+        transporter.sendMail(mailOptions, function (error, info)
+        {
+          if (error)
+          { console.log(error); } 
+          else 
+          {
+              let MobileBody = "New visitor information : ";
+              MobileBody += "Name : " + data.name + " " +
+              " Email : " + data.email_id + " " +
+              "Mobile Number : " + data.mobile_no + " " +
+              " Check In Date Time :" + data.checkin;
+              
+              client.messages.create({
+                  body: MobileBody,
+                  from: config.twilio.from_number,
+                  to: config.twilio.to_number,
+              },
+              function (error, info){
+                  if (error) 
+                  { console.log(error); } 
+                  else 
+                  { console.log("Message sent: " + info); }
               }
-            }
-          );
-          //.then(message => console.log(message.sid));
-
-          console.log("Email sent: " + info.response);
-        }
+            );
+            //.then(message => console.log(message.sid));
+            console.log("Email sent: " + info.response);
+         }
       });
     });
 
@@ -142,14 +132,11 @@ app.post("/save", (req, res) => {
   });
 });
 
-//route for update data
+//***Updating Checkout Time***
+
 app.post("/update", (req, res) => {
   //console.log(req.body.checkout);
-  let sql =
-    "UPDATE visitors SET checkout='" +
-    req.body.checkout +
-    "' WHERE id=" +
-    req.body.id;
+  let sql = "UPDATE visitors SET checkout='" + req.body.checkout + "' WHERE id=" + req.body.id;
   let query = conn.query(sql, (err, results) => {
     if (err) throw err;
 
@@ -175,18 +162,19 @@ app.post("/update", (req, res) => {
         html: htmlBody,
       };
       transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log("Email sent: " + info.response);
-        }
+        if (error) 
+        { console.log(error); } 
+        else 
+        { console.log("Email sent: " + info.response);}
       });
     });
     res.redirect("/");
   });
 });
-//====================================================================================================
-//route for delete data
+
+
+//*** Deleting any User Record***
+
 app.post("/delete", (req, res) => {
   let sql = "DELETE FROM visitors WHERE id=" + req.body.product_id + "";
   let query = conn.query(sql, (err, results) => {
@@ -194,6 +182,7 @@ app.post("/delete", (req, res) => {
     res.redirect("/");
   });
 });
+//__________________________________________________________________________________________________________________
 
 //server listening
 app.listen(8000, () => {
