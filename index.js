b0edda214ff195255c801843cb6913fd5e01700a
Lakshay Sharma <laksh1013shar@gmail.com>
2021-02-06 00:08:59 +0530

    mysql fixes

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const conn = mysql.createConnection({
   user: config.database.username,
   password: config.database.password,
   database: config.database.database_name,
-  port:config.database.port,
+  port:8889,
   socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
 });
 
